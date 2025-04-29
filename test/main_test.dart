e0bf20e4f5af10143352fc6888e4de336ee78871
RFer7935 <muhammadrioferdianto8@gmail.com>
2025-04-29 23:05:55 +0700

    add minor

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,21 +1,58 @@
-import 'package:flutter/material.dart';
-import 'widget_test.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  runApp(const MyApp());
+// Mock Item model for testing
+class Item {
+  String id;
+  String name;
+
+  Item({required this.id, required this.name});
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() {
+  group('CRUD Operations Test', () {
+    late List<Item> items;
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Test API',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const TestApiPage(), // Menggunakan TestApiPage sebagai halaman utama
-    );
-  }
+    setUp(() {
+      items = [];
+    });
+
+    test('Create - Should add new item to list', () {
+      // Create operation
+      items.add(Item(id: '1', name: 'Test Item'));
+
+      expect(items.length, 1);
+      expect(items.first.name, 'Test Item');
+    });
+
+    test('Read - Should retrieve item from list', () {
+      // Setup
+      items.add(Item(id: '1', name: 'Test Item'));
+
+      // Read operation
+      Item? foundItem = items.firstWhere((item) => item.id == '1');
+
+      expect(foundItem.name, 'Test Item');
+    });
+
+    test('Update - Should modify existing item', () {
+      // Setup
+      items.add(Item(id: '1', name: 'Test Item'));
+
+      // Update operation
+      int index = items.indexWhere((item) => item.id == '1');
+      items[index] = Item(id: '1', name: 'Updated Item');
+
+      expect(items[index].name, 'Updated Item');
+    });
+
+    test('Delete - Should remove item from list', () {
+      // Setup
+      items.add(Item(id: '1', name: 'Test Item'));
+
+      // Delete operation
+      items.removeWhere((item) => item.id == '1');
+
+      expect(items.length, 0);
+    });
+  });
 }
